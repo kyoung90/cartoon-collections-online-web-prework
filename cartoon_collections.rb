f7c5db9e199f0742746cda614e59c7d3a49da051
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-11 16:10:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,9 @@ def long_planeteer_calls(arr)# code an argument here
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
