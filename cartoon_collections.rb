73929d13ee3a2d69c99c1ed1e84ea8895e175d67
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-11 16:09:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,13 @@ end
 
 def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  arr.each do |ele|
+    if ele.length > 4
+      return true
+    end 
+  end 
   
+  return false
 end
 
 def find_the_cheese# code an argument here
